8394a2f4d3d5d0048d5ca5c1aeea3a9cc05e8cdb
Dmytro Kovalevskyi <dimakovalevskyi@gmail.com>
2017-09-09 22:24:11 +0300

    move progress component on top

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ValuesService } from './../values.service'
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    protected values: ValuesService
+  ) {
+    this.values.getSubscription().subscribe((data) => {
+      this.color = data.isValid ? 'primary' : 'warn';
+    })
   }
+
+  color: string = 'primary';
 
 }
